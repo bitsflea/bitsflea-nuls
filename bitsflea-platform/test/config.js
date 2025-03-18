592875d7f6854fbb739f7bd6debe95dfdad94ecf
xiangxn <xiangxn@163.com>
2025-03-18 16:23:14 +0800

    update

--- bitsflea-platform/test/config.js
+++ bitsflea-platform/test/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 import { NULSAPI } from "nuls-api-v2";
 
 export const env = process.env;
-export const contract = "NULSd6HgsKDNSpGc8bs8QZ1BoXtPZX6X9EHFt";
-export const sdk = new NULSAPI({ rpcURL: "https://api.nuls.io/jsonrpc", isBeta: false, accountPri: env.KEY_SENDER });
+export const contract = "tNULSeBaMzYcrWNadQdCunL8JLhKXrvVzDGrFE";
+export const sdk = new NULSAPI({ rpcURL: "https://beta.api.nuls.io/jsonrpc", isBeta: true, accountPri: env.KEY_SENDER });
